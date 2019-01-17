e26927bd1b311a476401f1b7c8dc8ceca2ff1527
Brian Van Klaveren <bvan@slac.stanford.edu>
2019-01-17 12:50:46 -0800

    Normalize ordering in code

--- oauthproxy.go
+++ oauthproxy.go
@@ -862,8 +862,8 @@ func (p *OAuthProxy) Authenticate(rw http.ResponseWriter, req *http.Request) int
 		var ok bool
 		if ok, err = p.provider.RefreshSessionIfNeeded(session); err != nil {
 			log.Printf("%s removing session. error refreshing access token %s %s", remoteAddr, err, session)
-			clearSession = true
 			session = nil
+			clearSession = true
 		} else if ok {
 			saveSession = true
 			revalidated = true
@@ -877,11 +877,11 @@ func (p *OAuthProxy) Authenticate(rw http.ResponseWriter, req *http.Request) int
 		clearSession = true
 	}
 
-	if saveSession && !revalidated && session != nil && session.AccessToken != "" {
+	if session != nil && saveSession && !revalidated && session.AccessToken != "" {
 		if !p.provider.ValidateSessionState(session) {
 			log.Printf("%s removing session. error validating %s", remoteAddr, session)
-			saveSession = false
 			session = nil
+			saveSession = false
 			clearSession = true
 		}
 	}
@@ -893,7 +893,7 @@ func (p *OAuthProxy) Authenticate(rw http.ResponseWriter, req *http.Request) int
 		clearSession = true
 	}
 
-	if saveSession && session != nil {
+	if session != nil && saveSession {
 		err = p.SaveSession(rw, req, session)
 		if err != nil {
 			log.Printf("%s %s", remoteAddr, err)
