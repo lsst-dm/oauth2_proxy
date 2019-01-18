d6f8f6173e6bd9ab86a7ea959c1bd50e9cc21154
Brian Van Klaveren <bvan@slac.stanford.edu>
2019-01-17 17:00:09 -0800

    Set IDToken for Verified JWT requests

--- oauthproxy.go
+++ oauthproxy.go
@@ -1038,6 +1038,7 @@ func (p *OAuthProxy) GetJwtSession(req *http.Request) (*providers.SessionState, 
 
 		session = &providers.SessionState{
 			AccessToken:  rawBearerToken,
+			IDToken:      rawBearerToken,
 			RefreshToken: "",
 			ExpiresOn:    bearerToken.Expiry,
 			Email:        claims.Email,
